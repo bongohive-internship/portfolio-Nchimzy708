f3722a50099dee29259d388911420bad734cb1d2
nchimunya Banda <nchimzy708@gmail.com>
2019-06-16 00:58:57 +0200

    made my portfolio

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+import myPic from './1.jpeg';
 import './App.css';
+import hobbies from './components/hobbies';
+import './animate.css';
 
 function App() {
   return (
     <div className="App">
+      
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://bongohive.co.zm"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Welcome to BongoHive, hoping you can join our dev team.
-        </a>
+      <div class="main">
+              <ul>
+                <li><a href="#" >soft skills</a></li>
+                <li><a href="hobbies">hobbies</a></li>
+                <li><a href="gallery.html">fears</a></li>
+                <li><a href="#">fav programming language</a></li>
+                <li><a href="#">schools Attended</a></li>
+                </ul>
+            
+          </div>
+        <img src={myPic} className="App-logo" alt="logo" />
+       <h1 class="animated infinite bounce slower">
+         Welcome to my Portfolio 
+       </h1>
+       <ol>
+          <li> My Name is Nchimunya Banda </li>
+          
+       </ol>
       </header>
     </div>
   );
---
+++ src/components/hobbies.js
@@ -0,0 +1,12 @@
+import React from 'react';
+function hobbies() {
+    return (
+      <div className="hbb">
+        <header className="hbb-header">
+        <h1>hobbies</h1>
+        </header>
+      </div>
+    );
+  }
+  
+  export default hobbies;
